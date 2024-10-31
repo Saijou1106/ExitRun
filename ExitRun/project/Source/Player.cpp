2e741ebbf0acd824be55cc3c3431581d7a3fc9e8
saijou <>
2024-10-31 12:24:57 +0900

    プレイヤージャンプ操作追加

--- ExitRun/project/Source/Player.cpp
+++ ExitRun/project/Source/Player.cpp
@@ -1,18 +1,21 @@
 #include "Player.h"
 #include "DxLib.h"
+#include "TitleScene.h"
+#include "PlayScene.h"
 
-int main() {
 
-}
-Player::Player() // : playerX(320), playerY(240), jumpCount(0), grounded(true), maxJump(2),jumpPower(-15), gravity(1)
+Player::Player() 
 {
-	playerX = 320;
-	playerY = 240;
+	hImage = LoadGraph("data/run1.png");
+	playerX = 120;
+	playerY = 200;
 	jumpCount = 0;
+	ground = 200;
 	grounded = true;
-	maxJump = 2;
-	jumpPower = -15;
-	gravity = 1;
+	maxJump = 2;   //最大ジャンプ回数
+	jumpPower = 5;//ジャンプ力
+	gravity = 1;   //重力
+	velocityY = 0; //Y方向の速度
 
 }
 
@@ -20,38 +23,61 @@ Player::Player() // : playerX(320), playerY(240), jumpCount(0), grounded(true), 
 
 void Player::Update()
 {
-	//スペースキーでジャンプ
-	if (CheckHitKey(KEY_INPUT_SPACE) && jumpCount < maxJump) {
-		Jump();
+
+	//Y座標の更新（上に動かすために減算）
+	playerY -= velocityY;
+
+
+	//初期状態では重力を適用しない
+	//if (grounded) {
+		//velocityY = 0; //地面にいる時は速度を0に設定
+	//}
+	
+
+	//地面にいない時だけ重力を適用
+	if(!grounded){
+		velocityY -= gravity;
 	}
 
-	//重力を適用
-	velocityY += gravity;
-	playerY = velocityY;
-
-	//地面に着地した時の処理(仮にY座標400が地面だとして)
-	if (playerY >= 400) {
-		playerY = 400;
-		velocityY = 0;
+	
+	//地面に着地した時の処理
+	if (playerY >= ground) {
+		playerY = ground;
+		velocityY = 0; //Yの速度0にする
 		onGround();
 	}
 
-	//地面にいないときはジャンプ回数を制御
-	if (!isOnGround()) {
-		grounded = false;
+	//スペースキーでジャンプ
+	if (CheckHitKey(KEY_INPUT_SPACE)) {
+		//地面にいるか、ジャンプ回数が１回のとき
+		if (grounded || (jumpCount <= maxJump)) {
+			Jump();//ジャンプ処理
+		}
+		
 	}
+//ジャンプ2回したらそれ以上ジャンプできないようにする
+	if (jumpCount >= maxJump) {
+		jumpCount = maxJump;
+	}
+
+
+
+	
+	//地面にいないとき（ジャンプ中）にジャンプカウントが1だったらもう一度ジャンプできる
+	
 }
 
 void Player::Draw()
 {
 	//プレイヤーを矩形で表現（座標：　x, y, サイズ：32×32）
-	DrawBox(playerX - 16, playerY - 16, playerX + 16, playerY + 16, GetColor(255, 255, 255), TRUE);
+	//DrawBox(playerX - 16, playerY - 16, playerX + 16, playerY + 16, GetColor(255, 255, 255), TRUE);
+	DrawGraph(playerX, playerY, hImage, TRUE);
 }
 
 //ジャンプ処理
 void Player::Jump()
 {
-	velocityY = jumpPower; //ジャンプ力を速度に反映
+	velocityY = jumpPower; //上方向に力を加える
 	jumpCount++;		   //ジャンプ回数を増やす
 	grounded = false;      //ジャンプ中は地面にいないのでfalse
 
--- ExitRun/project/Source/Player.h
+++ ExitRun/project/Source/Player.h
@@ -7,11 +7,14 @@ private:
 	int playerX;		//プレイヤーのX座標
 	int playerY;		//プレイヤーのY座標
 	float velocityY;	//Y方向の速度(垂直方向)
-	bool grounded;      //地面にいるかどうか	
+	int ground;         //地面の座標
+	bool grounded;      //地面にいるかどうか
 	int maxJump;        //最大ジャンプ回数（２回）
 	int jumpCount;		//現在のジャンプ回数
 	int jumpPower;      //ジャンプ力
 	int gravity;		//重力
+
+	int hImage;//画像
 
 public:
 	Player();
