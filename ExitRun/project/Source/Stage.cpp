f7bf1254673ec916b3997e09368dd783da687649
HOMEGTUNE\nao <sendai@hanamaru.games>
2025-01-18 23:22:17 +0900

    ステージを切り替えられるようにしました。

--- ExitRun/project/Source/Stage.cpp
+++ ExitRun/project/Source/Stage.cpp
@@ -3,6 +3,9 @@
 #include "Player.h"
 #include "Shield.h"
 #include "CsvReader.h"
+#include "Screen.h"
+#include "../ImGui/imgui.h"
+
 //	dataフォルダにあるcsvを読み込む方法ならこれらは不要(HORIKOSHI Masahiro)
 #if false
 #include "stage1.h"
@@ -54,188 +57,166 @@ const std::string mapFile[5] =
 
 Stage::Stage()
 {
-	//CsvReader reader("data/仮.csv");
-	int mapNo = 1;
-	CsvReader reader(mapFile[mapNo]);		//	Map番号に該当するmapファイルを読み込む
-	//　stage1からstage2への画面切り替えを作らなきゃいけない
-	
+
 	backGroundImage = LoadGraph("data/background2.png");
 	// 床用の画像読み込み
 	floorImage = LoadGraph("data/floor2.png");
-	
+
 	//	ブロック用画像読み込み(HORIKOSHI Masahiro)
 	blockImage = LoadGraph("data/floor1.png");
 	//	背景chip用画像読み込み(HORIKOSHI Masahiro)
 	backChipImage = LoadGraph("data/backchip.png");
 
-	int a = reader.GetLines();
-	for (int y = 0; y < reader.GetLines(); y++) {
+	mapNo = 0;
+	nextMapNo = -1;
+	CreateStage(mapNo);
+	scroll = 2;
+}
 
-		if (y < 12)
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map1[y][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 15 ) && (y < 27))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map2[y-15][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 30) && (y < 42))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map3[y-30][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 45) && (y < 57))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map4[y-45][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 60) && (y < 72))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map5[y-60][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 75) && (y < 87))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map6[y-75][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 90) && (y < 102))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map7[y-90][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 105) && (y < 117))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map8[y-105][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 120) && (y < 132))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map9[y-120][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 135) && (y < 147))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map10[y-135][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 150) && (y < 162))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map11[y-150][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 165) && (y < 177))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map12[y-165][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 180) && (y < 192))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map13[y-180][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 195) && (y < 207))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map14[y-195][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 210) && (y < 222))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map15[y-210][x] = reader.GetInt(y, x);
-			}
-		}
-		if ((y >= 225) && (y < 237))
-		{
-			for (int x = 0; x < reader.GetColumns(y); x++) {
-				map16[y-225][x] = reader.GetInt(y, x);
-			}
-		}
-#if false
-		for (int x = 0; x < reader.GetColumns(y); x++) {
-			map[y][x] = reader.GetInt(y, x);
-		}
-#endif
+Stage::~Stage()
+{
+}
+
+void Stage::Update()
+{
+	int sc = (scroll / CHIP_SIZE) % WORLD_WIDTH;
+	if (sc > WIDTH * 16 - CHIP_SIZE && nextMapNo>=0) {
+ 		mapNo = nextMapNo;
+		nextMapNo = -1;
+		CreateStage(mapNo);
 	}
+}
 
-	//	map1～map16を、mapにコピーする(ただし、横つながり)
-	for (int i = 0; i < 16; i++)
+void Stage::Draw()
+{
+	int drawScroll = scroll % (WORLD_WIDTH*CHIP_SIZE);
+	// ブロック = 2
+	for (int i = 0; i < WORLD_WIDTH; i++)
 	{
-		for (int y = 0; y < HEIGHT; y++)
-		{
-			for (int x = 0; x < WIDTH; x++)
+		int x = ((i+WORLD_WIDTH+1) * CHIP_SIZE-drawScroll) % (WORLD_WIDTH*CHIP_SIZE) - CHIP_SIZE;
+		if (x>Screen::WIDTH) {
+			continue;
+		}
+		for (int j = 0; j < HEIGHT; j++) {
+			int y = j * CHIP_SIZE;
+#if false
+			if (map[j][i] == 1)
+				DrawRectGraph(x - scroll,y, 0, 0, 40, 40, floorImage, TRUE);
+#endif
+			//	if分でもできるが、複数の設定を行う場合はswitch分のほうがお勧め(HORIKOSHI Masahiro)
+			switch (map[j][i])
 			{
-				switch (i)
-				{
-					case 0:
-						map[y][x] = map1[y][x];
-						break;
-					case 1:
-						map[y][x+(WIDTH * i)] = map2[y][x];
-						break;
-					case 2:
-						map[y][x + (WIDTH * i)] = map3[y][x];
-						break;
-					case 3:
-						map[y][x + (WIDTH * i)] = map4[y][x];
-						break;
-					case 4:
-						map[y][x + (WIDTH * i)] = map5[y][x];
-						break;
-					case 5:
-						map[y][x + (WIDTH * i)] = map6[y][x];
-						break;
-					case 6:
-						map[y][x + (WIDTH * i)] = map7[y][x];
-						break;
-					case 7:
-						map[y][x + (WIDTH * i)] = map8[y][x];
-						break;
-					case 8:
-						map[y][x + (WIDTH * i)] = map9[y][x];
-						break;
-					case 9:
-						map[y][x + (WIDTH * i)] = map10[y][x];
-						break;
-					case 10:
-						map[y][x + (WIDTH * i)] = map11[y][x];
-						break;
-					case 11:
-						map[y][x + (WIDTH * i)] = map12[y][x];
-						break;
-					case 12:
-						map[y][x + (WIDTH * i)] = map13[y][x];
-						break;
-					case 13:
-						map[y][x + (WIDTH * i)] = map14[y][x];
-						break;
-					case 14:
-						map[y][x + (WIDTH * i)] = map15[y][x];
-						break;
-					case 15:
-						map[y][x + (WIDTH * i)] = map16[y][x];
-						break;
-				}
+				//	床
+				case 1:
+					DrawRectGraph(x,y, 0, 0, CHIP_SIZE, CHIP_SIZE, floorImage, TRUE);
+					break;
+				//	ブロック
+				case 2:
+					DrawRectGraph(x,y, 0, 0, CHIP_SIZE, CHIP_SIZE, floorImage, TRUE);
+					break;
+				//	背景Chip(敵配置したところにも背景チップを置くように変更...HORIKOSHI Masahiro)
+				default:
+					DrawRectGraph(x,y, 0, 0, CHIP_SIZE, CHIP_SIZE, backChipImage, TRUE);
+					break;
 			}
 		}
 	}
+#if false
+	DrawGraph(backGroundX - scroll , backGroundY, backGroundImage , TRUE);
 
+	DrawGraph(floorX - scroll , floorY, floorImage, TRUE);
+#endif
+}
+
+int Stage::IsWallUp(VECTOR2 pos)
+{
+	int i = ((int)pos.x / CHIP_SIZE) % WORLD_WIDTH;
+	int j = pos.y / CHIP_SIZE;
+	if (i < 0 || j < 0 || j >= HEIGHT)
+		return 0;
+	if (map[j][i] == 1 || map[j][i] == 2)
+	{
+		int push = 64 - (int)pos.y % CHIP_SIZE;
+		return push;
+	}
+	return 0;
+}
+
+int Stage::IsWallDown(VECTOR2 pos)
+{
+	int i = ((int)pos.x / CHIP_SIZE) % WORLD_WIDTH;
+	int j = pos.y / CHIP_SIZE;
+	if (i < 0 || j < 0 || j >= HEIGHT)
+		return 0;
+	if (map[j][i] == 1 || map[j][i] == 2)
+	{
+		int push = (int)pos.y % CHIP_SIZE + 1;
+		return push;
+	}
+	return 0;
+}
+
+int Stage::IsWallRight(VECTOR2 pos)
+{
+	int i = ((int)pos.x / CHIP_SIZE) % WORLD_WIDTH;
+	int j = pos.y / CHIP_SIZE;
+	if (i < 0 || j < 0 || j >= HEIGHT)
+		return 0;
+	if (map[j][i] == 1 || map[j][i] == 2)
+	{
+		int push = (int)pos.x % CHIP_SIZE + 1;
+		return push;
+	}
+	return 0;
+}
+
+int Stage::IsWallLeft(VECTOR2 pos)
+{
+	int i = ((int)pos.x / CHIP_SIZE) % WORLD_WIDTH;
+	int j = pos.y / CHIP_SIZE;
+	if (i < 0 || j < 0 || j >= HEIGHT)
+		return 0;
+	if (map[j][i] == 1 || map[j][i] == 2)
+	{
+		int push = 64 - (int)pos.x % CHIP_SIZE;
+		return push;
+	}
+	return 0;
+}
+
+void Stage::CreateStage(int st)
+{
+
+	std::list<Enemy*> enemies = FindGameObjects<Enemy>();
+	for (Enemy* e : enemies) {
+		e->DestroyMe();
+	}
+	std::list<Shield*> shields = FindGameObjects<Shield>();
+	for (Shield* s : shields) {
+		s->DestroyMe();
+	}
+
+	for (int i = 0; i < WORLD_WIDTH; i++)
+	{
+		for (int j = 0; j < HEIGHT; j++)
+		{
+			map[j][i] = 0;
+			map[0][i] = 1;
+			map[HEIGHT-1][i] = 1;
+		}
+	}
+
+	//CsvReader reader("data/仮.csv");
+	CsvReader reader(mapFile[st]);		//	Map番号に該当するmapファイルを読み込む
+	//　stage1からstage2への画面切り替えを作らなきゃいけない
+	int a = reader.GetLines();
+	for (int page = 0; page < 16; page++) {
+		for (int y = 0; y < HEIGHT; y++) {
+			for (int x = 0; x < WIDTH; x++) {
+				map[y][page * WIDTH + x] = reader.GetInt(page * 15 + y, x);
+			}
+		}
+	}
 	for (int j = 0; j < HEIGHT; j++)
 	{
 		for (int i = 0; i < WIDTH; i++)
@@ -252,199 +233,93 @@ Stage::Stage()
 			switch (map[j][i])
 			{
 				// GroundEnemy1
-				case 3:
+			case 3:
+			{
+				GroundEnemy1* enemy1 = Instantiate<GroundEnemy1>();
+				enemy1->position.x = i * CHIP_SIZE;
+				enemy1->position.y = j * CHIP_SIZE;
+			}
+			break;
+
+			// GroundEnemy2
+			case 4:
+			{
+				GroundEnemy2* enemy2 = Instantiate<GroundEnemy2>();
+				enemy2->position.x = i * CHIP_SIZE;
+				enemy2->position.y = j * CHIP_SIZE;
+			}
+			break;
+
+			// SkyEnemey1
+			case 5:
+			{
+				SkyEnemy1* enemy1 = Instantiate<SkyEnemy1>();
+				enemy1->position.x = i * CHIP_SIZE;
+				enemy1->position.y = j * CHIP_SIZE;
+			}
+			break;
+
+			// SkyEnemey2
+			case 6:
+			{
+				SkyEnemy2* enemy2 = Instantiate<SkyEnemy2>();
+				enemy2->position.x = i * CHIP_SIZE;
+				enemy2->position.y = i * CHIP_SIZE;
+			}
+			break;
+
+			//	プレイヤー
+			case 9:
+			{
+				Player* p = Instantiate<Player>();
+				p->position.x = i * CHIP_SIZE;	//	なぜ100足している？(HORIKOSHI Masahiro)
+				p->position.y = j * CHIP_SIZE;	//	なぜ100足している？(HORIKOSHI Masahiro)
+			}
+			break;
+			//	ランダム敵(仮に7としている)
+			case 7:
+			{
+				int rand = GetRand(3);		//	0-3の乱数
+				//	乱数の結果で敵の出現を変える
+				switch (rand)
+				{
+				case 0:
 				{
 					GroundEnemy1* enemy1 = Instantiate<GroundEnemy1>();
 					enemy1->position.x = i * CHIP_SIZE;
 					enemy1->position.y = j * CHIP_SIZE;
-				}
+			}
 				break;
-
-				// GroundEnemy2
-				case 4:
+				case 1:
 				{
 					GroundEnemy2* enemy2 = Instantiate<GroundEnemy2>();
 					enemy2->position.x = i * CHIP_SIZE;
 					enemy2->position.y = j * CHIP_SIZE;
 				}
 				break;
-
-				// SkyEnemey1
-				case 5:
+				case 2:
 				{
-					SkyEnemy1* enemy1 = Instantiate<SkyEnemy1>();
-					enemy1->position.x = i * CHIP_SIZE;
-					enemy1->position.y = j * CHIP_SIZE;
+					SkyEnemy1* enemy3 = Instantiate<SkyEnemy1>();
+					enemy3->position.x = i * CHIP_SIZE;
+					enemy3->position.y = j * CHIP_SIZE;
 				}
 				break;
-
-				// SkyEnemey2
-				case 6:
+				case 3:
 				{
-					SkyEnemy2* enemy2 = Instantiate<SkyEnemy2>();
-					enemy2->position.x = i * CHIP_SIZE;
-					enemy2->position.y = i * CHIP_SIZE;
+					SkyEnemy2* enemy4 = Instantiate<SkyEnemy2>();
+					enemy4->position.x = i * CHIP_SIZE;
+					enemy4->position.y = j * CHIP_SIZE;
 				}
 				break;
-
-				//	プレイヤー
-				case 9:
-					{
-						Player* p = Instantiate<Player>();
-						p->position.x = i * CHIP_SIZE;	//	なぜ100足している？(HORIKOSHI Masahiro)
-						p->position.y = j * CHIP_SIZE;	//	なぜ100足している？(HORIKOSHI Masahiro)
-					}
-					break;
-				//	ランダム敵(仮に7としている)
-				case 7:
-					{
-						int rand = GetRand(3);		//	0-3の乱数
-						//	乱数の結果で敵の出現を変える
-						switch (rand)
-						{
-							case 0:
-								{
-									GroundEnemy1* enemy1 = Instantiate<GroundEnemy1>();
-									enemy1->position.x = i * CHIP_SIZE;
-									enemy1->position.y = j * CHIP_SIZE;
-								}
-								break;
-							case 1:
-								{
-									GroundEnemy2* enemy2 = Instantiate<GroundEnemy2>();
-									enemy2->position.x = i * CHIP_SIZE;
-									enemy2->position.y = j * CHIP_SIZE;
-								}
-								break;
-							case 2:
-								{
-									SkyEnemy1* enemy3 = Instantiate<SkyEnemy1>();
-									enemy3->position.x = i * CHIP_SIZE;
-									enemy3->position.y = j * CHIP_SIZE;
-								}
-								break;
-							case 3:
-								{
-									SkyEnemy2* enemy4 = Instantiate<SkyEnemy2>();
-									enemy4->position.x = i * CHIP_SIZE;
-									enemy4->position.y = j * CHIP_SIZE;
-								}
-								break;
-						}
-					}
-					break;
-			}
+		}
+	}
+			break;
+}
 		}
 	}
 	backGroundX = 0;
 	backGroundY = 0;
 	floorX = 300;
 	floorY = 520;
-	
-	scroll = 2;
-	//for (int j = 0; j < HEIGHT; j++) {
-	//	for (int i = 0; i < WIDTH; i++) {
-	//		if (map[j][i] == 9) {//9だったらプレイヤー
-	//			Player* p = Instantiate<Player>();
-	//			p->position.x = i * 64;
-	//			p->position.y = j * 64;
-	//		}
-	//	}
-	//}
-	//scroll = 0;
-}
-
-Stage::~Stage()
-{
-}
-
-void Stage::Draw()
-{
-	/*for (int j = 0; j < HEIGHT; j++) {
-
-	}*/
-	// ブロック = 2
-	for (int j = 0; j < HEIGHT; j++)
-	{
-		int y = j * CHIP_SIZE;
-		//for (int i = 0; i < WIDTH; i++)
-		for (int i = 0; i < WORLD_WIDTH; i++)
-		{
-			int x = i * CHIP_SIZE;
-#if false
-			if (map[j][i] == 1)
-				DrawRectGraph(x - scroll,y, 0, 0, 40, 40, floorImage, TRUE);
-#endif
-			//	if分でもできるが、複数の設定を行う場合はswitch分のほうがお勧め(HORIKOSHI Masahiro)
-			switch (map[j][i])
-			{
-				//	床
-				case 1:
-					DrawRectGraph(x - scroll,y, 0, 0, CHIP_SIZE, CHIP_SIZE, floorImage, TRUE);
-					break;
-				//	ブロック
-				case 2:
-					DrawRectGraph(x - scroll,y, 0, 0, CHIP_SIZE, CHIP_SIZE, floorImage, TRUE);
-					break;
-				//	背景Chip(敵配置したところにも背景チップを置くように変更...HORIKOSHI Masahiro)
-				default:
-					DrawRectGraph(x - scroll,y, 0, 0, CHIP_SIZE, CHIP_SIZE, backChipImage, TRUE);
-					break;
-			}
-		}
-	}
-#if false
-	DrawGraph(backGroundX - scroll , backGroundY, backGroundImage , TRUE);
-
-	DrawGraph(floorX - scroll , floorY, floorImage, TRUE);
-#endif
-}
-
-int Stage::IsWallUp(VECTOR2 pos)
-{
-	int i = pos.x / CHIP_SIZE;
-	int j = pos.y / CHIP_SIZE;
-	if (map[j][i] == 1 || map[j][i] == 2)
-	{
-		int push = 64 - (int)pos.y % CHIP_SIZE;
-		return push;
-	}
-	return 0;
-}
-
-int Stage::IsWallRight(VECTOR2 pos)
-{
-	int i = pos.x / CHIP_SIZE;
-	int j = pos.y / CHIP_SIZE;
-	if (map[j][i] == 1 || map[j][i] == 2)
-	{
-		int push = (int)pos.x % CHIP_SIZE + 1;
-		return push;
-	}
-	return 0;
-}
-
-int Stage::IsWallLeft(VECTOR2 pos)
-{
-	int i = pos.x / CHIP_SIZE;
-	int j = pos.y / CHIP_SIZE;
-	if (map[j][i] == 1 || map[j][i] == 2)
-	{
-		int push = 64 - (int)pos.x % CHIP_SIZE;
-		return push;
-	}
-	return 0;
-}
-
-int Stage::IsWallDown(VECTOR2 pos)
-{
-	int i = pos.x / CHIP_SIZE;
-	int j = pos.y / CHIP_SIZE;
-	if (map[j][i] == 1 || map[j][i]==2)
-	{
- 		int push = (int)pos.y % CHIP_SIZE + 1;
-		return push;
-	}
-	return 0;
 }
 
--- ExitRun/project/Source/Stage.h
+++ ExitRun/project/Source/Stage.h
@@ -7,7 +7,7 @@
 //const int HEIGHT = 234;
 const int WIDTH = 18;
 const int HEIGHT = 12;
-const int WORLD_WIDTH = WIDTH * 16;
+const int WORLD_WIDTH = WIDTH * 16+20;
 
 class Stage : public GameObject
 {
@@ -15,6 +15,7 @@ class Stage : public GameObject
 public:
 	Stage();
 	~Stage();
+	void Update() override;
 	void Draw();
 	int IsWallUp(VECTOR2 pos);
 	int IsWallDown(VECTOR2 pos);
@@ -26,24 +27,6 @@ public:
 	int backGroundImage;
 	int backGroundX;
 	int backGroundY;
-#if false
-	int map1;
-	int map2;
-	int map3;
-	int map4;
-	int map5;
-	int map6;
-	int map7;
-	int map8;
-	int map9;
-	int map10;
-	int map11;
-	int map12;
-	int map13;
-	int map14;
-	int map15;
-	int map16;
-#endif
 	int floorImage;
 	int floorX;
 	int floorY;
@@ -55,22 +38,9 @@ public:
 
 	//	dataフォルダにあるcsvを読み込む方法ならここで定義(HORIKOSHI Masahiro)
 	int map[HEIGHT][WORLD_WIDTH];
-
-	int map1[HEIGHT][WIDTH];
-	int map2[HEIGHT][WIDTH];
-	int map3[HEIGHT][WIDTH];
-	int map4[HEIGHT][WIDTH];
-	int map5[HEIGHT][WIDTH];
-	int map6[HEIGHT][WIDTH];
-	int map7[HEIGHT][WIDTH];
-	int map8[HEIGHT][WIDTH];
-	int map9[HEIGHT][WIDTH];
-	int map10[HEIGHT][WIDTH];
-	int map11[HEIGHT][WIDTH];
-	int map12[HEIGHT][WIDTH];
-	int map13[HEIGHT][WIDTH];
-	int map14[HEIGHT][WIDTH];
-	int map15[HEIGHT][WIDTH];
-	int map16[HEIGHT][WIDTH];
+	int mapNo;
+	int nextMapNo;
+private:
+	void CreateStage(int st);
 };
 
